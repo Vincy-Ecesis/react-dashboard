636efb703012d9e43bfaf1d3048e06a62a3cadcc
Vincy <vincy.ecesis@gmail.com>
2023-12-08 19:27:04 +0530

    check

--- src/Components/AllTabs/FirstTab.js
+++ src/Components/AllTabs/FirstTab.js
@@ -1,25 +1,28 @@
 import React, { useState } from 'react';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
-
+import './FirstTab.css';
 const FirstTab = () => {
     return (
         <>
             <div>
+                <div>
+                    <h3>Your Web 2.0 domains</h3>
+                </div>
                 <Tabs
                     defaultActiveKey="profile"
                     id="uncontrolled-tab-example"
                     className="mb-3"
                 >
-                    <Tab eventKey="home" title="Home">
+                    <Tab eventKey="home" title="Active Domains">
                         Tab content for Home
                     </Tab>
-                    <Tab eventKey="profile" title="Profile">
+                    <Tab eventKey="profile" title="Your Domains">
                         Tab content for Profile
                     </Tab>
-                    <Tab eventKey="contact" title="Contact" disabled>
+                    {/* <Tab eventKey="contact" title="Contact" disabled>
                         Tab content for Contact
-                    </Tab>
+                    </Tab> */}
                 </Tabs>
             </div>
 
