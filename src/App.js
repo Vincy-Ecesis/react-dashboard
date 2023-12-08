fee18e87edfa157dbc49e198a8d9aa534288f3d9
Vincy <vincy.ecesis@gmail.com>
2023-12-08 17:35:50 +0530

    react-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  Routes,
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
+import { useState, useEffect, Fragment } from 'react';
+import Dashboard from './Components/Dashboard/dashboard';
+import Navbar from './Components/Navbar/navbar';
+import Header from './Components/Header/header';
+import ManageDomain from './Components/Manage Domains/managedomains'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <Router>
+        
+      <div className='fixed-header'>
+      <Header/>
+      <Navbar/>
+      </div>
+        <Routes>
+          <Fragment>
+            {/* <Route path="/" element={<Homepage  />} exact /> */}
+            <Route path="/" element={<Dashboard  />} exact />
+            <Route path="/domains" element={<ManageDomain  />} exact />
+          </Fragment>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/AllTabs/SecondTab.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+
+const SecondTab = () => {
+    return(
+        <>
+        <div>Heloooooo second</div>
+        </>
+    )
+}
+
+export default SecondTab;
